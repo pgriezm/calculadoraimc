dbff4df1998184acb129ac2eee34de0c994cc1c0
calculadoraIMC <peedroo.a@gmail.com>
2025-01-09 14:06:04 +0100

    mostrar mensagem "preencha os campos"

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -25,15 +26,34 @@ class MainActivity : AppCompatActivity() {
 
 
         btnCAlcular.setOnClickListener {
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
+
+
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
+
+            if (pesoStr == "" || alturaStr == "") {
+                //Mostrar mensagem para o usuario
+                Snackbar
+                    .make(
+                        edtPeso,
+                        "Preencha todos os campos",
+                        Snackbar.LENGTH_LONG
+
+                    )
+                    .show()
+
+
+            }   else {
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
 
             val alturaQ2 = altura * altura
             val resultado = peso / alturaQ2
 
+
             println("Pedro ação do botão" + resultado)
 
-
+            }
 
         }
 
