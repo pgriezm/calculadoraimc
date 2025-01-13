8155e93104304e007ca1d04dee77c06285c0aff1
calculadoraIMC <peedroo.a@gmail.com>
2025-01-13 12:35:27 +0100

    finalização do Layout da segunda tela

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -17,12 +17,27 @@ class ResultActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
 
-        // o codigo abaixo é para trazer o conteudo da pagina anterior para essa pagina, nesse caso o KEU_RESULTADO_IMC
+        // o codigo abaixo é para trazer o conteudo da pagina anterior para essa pagina, nesse caso o KEY_RESULTADO_IMC
         val result = intent.getFloatExtra(KEY_RESULT_IMC, 0f)
         val tvResult = findViewById<TextView>(R.id.tv_result)
         val tvClassificacao = findViewById<TextView>(R.id.tv_classificacao)
 
         tvResult.text = result.toString()
+
+        var classifição: String? = if (result <= 18.5f){
+            "MAGREZA"
+        } else if (result > 18.5f && result <= 24.9f) {
+            "NORMAL"
+        } else if (result > 25f && result <= 29.9f) {
+            "SOBREPESO"
+        } else if (result > 30f && result <= 39.9f) {
+            "OBESIDADE"
+        } else {
+            "OBESIDADE GRAVE"
+        }
+
+
+        tvClassificacao.text = classifição
 
 
 
